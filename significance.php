c69bb666b0e82caaf95353e287a7462fe2d95d6e
Purahan Gupta <70795912+Purahan@users.noreply.github.com>
2023-08-21 23:24:49 +0530

    Uploading final files

---
+++ significance.php
@@ -0,0 +1,65 @@
+<?php
+  include('includes/functions.php');
+  page_header();
+  nav_bar();
+?>
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Significance of his Discoveries for Modern World</li>
+        </ol>
+        <h2>Significance of his Discoveries for Modern World</h2>
+
+      </div>
+    </section>
+
+    <section class="inner-page">
+      <div class="container">
+        <p>
+          Aryabhata's discoveries and contributions have had a lasting impact on the modern world, particularly in the fields of mathematics, astronomy, and science more broadly. Here's the significance of his discoveries for the modern world:
+        </p>
+
+        <h3 class="pt-3 pb-2">Numerical Notation and Zero</h3>
+        <p>
+          Aryabhata's introduction of the decimal place-value system and the concept of zero laid the foundation for the modern numeral system used universally today. This numerical notation system revolutionized mathematics, making arithmetic and calculations more efficient and accessible. The concept of zero was a fundamental step in the development of advanced mathematical operations and paved the way for algebra and higher mathematics.
+        </p>
+
+        <h3 class="pt-3 pb-2">Trigonometry and Geometry</h3>
+        <p>
+          Aryabhata's work in trigonometry and geometry provided valuable methods for calculating angles, distances, and areas. His development of trigonometric functions and geometric principles formed the basis for further advancements in these fields. Trigonometry, in particular, is essential in fields such as physics, engineering, and astronomy, enabling precise measurements and calculations.
+        </p>
+
+        <h3 class="pt-3 pb-2">Astronomy and Understanding the Cosmos</h3>
+        <p>
+          Aryabhata's heliocentric model of the solar system, while not immediately accepted in his time, was an early step toward understanding the true nature of planetary motion. His calculations for the lengths of the solar day and the year showed a remarkable understanding of astronomical phenomena. His insights into eclipses and celestial positions contributed to the overall understanding of the cosmos and influenced the development of modern astronomy.
+        </p>
+
+        <h3 class="pt-3 pb-2">Calendar Systems</h3>
+        <p>
+          Aryabhata's work on calendars, including the intercalary month and the concept of leap years, has implications for modern calendar systems. While his specific methods might not be directly applied today, his understanding of reconciling lunar and solar cycles is relevant to the ongoing development and adjustment of calendars to accurately reflect the passage of time.
+        </p>
+
+        <h3 class="pt-3 pb-2">Mathematical Rigor and Methodology</h3>
+        <p>
+          Aryabhata's systematic approach to mathematical and astronomical problems set a precedent for rigorous investigation and methodology. His emphasis on calculations, measurement, and analysis established a framework for scientific inquiry that is foundational to modern scientific research.
+        </p>
+
+        <h3 class="pt-3 pb-2">Cultural and Historical Impact</h3>
+        <p>
+          Aryabhata's work is an important part of India's intellectual history. His contributions have helped preserve and promote the rich heritage of Indian mathematics and science. His discoveries serve as a reminder of the enduring human quest for knowledge and understanding of the universe.
+        </p>
+        <br />
+        <p>
+          In summary, Aryabhata's discoveries and contributions continue to influence modern mathematics, science, and technology. His ideas have become integral to the way we approach numerical notation, algebra, trigonometry, astronomy, and scientific inquiry. His work not only advanced knowledge in his time but also laid the groundwork for the development of modern disciplines and methodologies that shape our understanding of the world today.
+        </p>
+      </div>
+    </section>
+
+  </main>
+
+<?php page_footer(); ?>
---
+++ time-calendar.php
@@ -0,0 +1,52 @@
+<?php
+  include('includes/functions.php');
+  page_header();
+  nav_bar();
+?>
+  <main id="main">
+
+    <!-- ======= Breadcrumbs ======= -->
+    <section id="breadcrumbs" class="breadcrumbs">
+      <div class="container">
+
+        <ol>
+          <li><a href="index.html">Home</a></li>
+          <li>Areas of Work</li>
+          <li>Time Calendars</li>
+        </ol>
+        <h2>Time Calendars</h2>
+
+      </div>
+    </section>
+
+    <section class="inner-page">
+      <div class="container">
+        <h3 class="pb-3" id="days">Length of the Solar Day</h3>
+        <p>
+            Aryabhata calculated the length of the solar day, which is the time interval between two successive occurrences of the Sun crossing the same meridian. He arrived at a value of 23 hours, 56 minutes, and 4.1 seconds, which is very close to the modern value of the sidereal day. This calculation demonstrates his accurate understanding of Earth's rotation.
+        </p>
+
+        <h3 class="pb-3 pt-5">Length of the Year</h3>
+        <p>
+            Aryabhata's work included calculations for the length of a year. He estimated the length of a tropical year (the time it takes for the Earth to complete one orbit around the Sun) to be approximately 365.3586805 days, which is quite close to the modern value of around 365.242190 days. This demonstrates his ability to make precise astronomical calculations.
+        </p>
+
+        <h3 class="pb-3 pt-5" id="calendar">Calendars</h3>
+        <p>
+            Aryabhata discussed various aspects of calendars, including the calculation of leap years. He proposed a calendar system that incorporated both solar and lunar cycles. His work aimed at reconciling the solar year with the lunar month to create a more accurate and practical calendar. This involved the introduction of an intercalary month to adjust for the difference between the two cycles.
+        </p>
+
+        <h3 class="pb-3 pt-5">Yuga System</h3>
+        <p>
+            Aryabhata introduced the concept of "yugas," which are cyclic time periods in Hindu cosmology. He divided time into four yugas: Krita Yuga (1,728,000 years), Treta Yuga (1,296,000 years), Dvapara Yuga (864,000 years), and Kali Yuga (432,000 years). These yugas constitute a "mahayuga" cycle of 4,320,000 years. Aryabhata's inclusion of these yugas in his work reflects his understanding of the cosmological and spiritual dimensions of time.
+        </p><br />
+
+        <p>
+            Aryabhata's insights into time and calendars were foundational in Indian astronomy and mathematics. While some of his calculations were not entirely accurate by modern standards, his work laid the groundwork for more precise calculations and improvements in calendar systems by later mathematicians and astronomers in India. His contributions helped shape the way time is measured and calendars are organized in various cultures around the world.
+        </p>
+      </div>
+    </section>
+
+  </main>
+
+<?php page_footer(); ?>
